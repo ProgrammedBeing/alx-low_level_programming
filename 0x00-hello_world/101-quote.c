cbd03e915c158770242048bbfb15d6f1f09ba75c
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-19 19:33:23 -0800

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (1);
+}
