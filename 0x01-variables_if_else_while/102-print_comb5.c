61c7329a1a57e1e3b162f43d1973f298cfcb548f
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-21 22:08:51 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,11 +18,11 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i != 98 && j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (i == 98 && j == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
