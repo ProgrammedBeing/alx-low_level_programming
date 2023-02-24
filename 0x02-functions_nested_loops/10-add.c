496f4401d99c513f96cff6d2b1f5202f47b96ad4
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-24 07:00:35 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @n: 1st Parameter
+ * @k: 2nd Parameter
+ * Return: Always 0 (success)
+ */
+
+int add(int n, int k)
+{
+	int sum = n + k;
+
+	return (sum);
+}
