67e77378be98a882a72024fcd43136d2e8bb418b
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-25 13:45:58 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: Always 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
