9c96c88a6ef0adef5e3ca41d60c0f82391ce4521
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-24 17:02:49 -0800

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ * @n: Parameter
+ * Return: Always 0 (success)
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+
+			printf(",  ");
+		}
+		putchar('\n');
+	}
+	else
+	{
+		for (; n <= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+
+			printf(",  ");
+		}
+		printf("\n");
+	}
+}
