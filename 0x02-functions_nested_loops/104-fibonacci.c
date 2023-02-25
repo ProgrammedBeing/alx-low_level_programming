8d40f0bb0c927eb8edcf6fe0bafc15a37d2908bd
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-25 06:47:31 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned int f1 = 1;
+	unsigned int f2 = 2;
+	unsigned int Nt = f1 + f2;
+	int i;
+
+	printf("%d, %d, ", f1, f2);
+	for (i = 3; i <= 98; i++)
+	{
+		printf("%d, ", Nt);
+		f1 = f2;
+		f2 = Nt;
+		Nt = f1 + f2;
+	}
+	putchar('\n');
+	return (0);
+}
