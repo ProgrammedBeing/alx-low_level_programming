b41cd030404828b3c53122f2571740abc1a4f419
ProgrammedBeing <kachirhaps@gmail.com>
2023-02-25 14:29:06 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: Parameter
+ * Return: Always 0 (success)
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+}
