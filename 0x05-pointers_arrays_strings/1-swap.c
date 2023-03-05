5e171b096643a6ce5d2ab5f2376e2d2f0be65d0e
ProgrammedBeing <kachirhaps@gmail.com>
2023-03-04 18:37:54 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ *
+ * @a: Pointer Parameter
+ * @b: Pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
