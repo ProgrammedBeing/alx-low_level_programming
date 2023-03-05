6deb5a8408d7ba3533867ce9790f91ec1627b4db
ProgrammedBeing <kachirhaps@gmail.com>
2023-03-05 10:05:11 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - Pointer function
+ *
+ * @dest: copied to
+ * @src: copied from
+ * Return: Memory address
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	return (dest);
+}
