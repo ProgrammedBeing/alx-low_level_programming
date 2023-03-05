885b80a45c63b6a820efa83fbede9c4fd811d02b
ProgrammedBeing <kachirhaps@gmail.com>
2023-03-05 00:36:26 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - Entry point
+ *
+ * @str: Pointer Parameter
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int i, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+		for (i = n; i < len; i++)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else
+	{
+		n = (len - 1) / 2;
+		for (i = n; i >= 0; i--)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
