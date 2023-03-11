3a92f875d0164918e55547b305d095a72242366b
ProgrammedBeing <kachirhaps@gmail.com>
2023-03-11 06:47:23 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ *
+ * @n: Inputted number
+ * Return: n * factorial(n - 1);
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return -1;
+	else if (n == 1)
+		return 1;
+	else
+		return (n * factorial(n - 1));
+}
