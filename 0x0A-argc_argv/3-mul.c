695b82cd2ccef76679e815354867144e6a3085ba
ProgrammedBeing <kachirhaps@gmail.com>
2023-03-13 13:02:03 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 3
+/**
+ * main - Entry point
+ *
+ * @argc: command line count
+ * @argv: a pointer to strings
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc <= SIZE)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
