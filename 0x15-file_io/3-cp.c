5c06e9f3ecdee05d7355324107d518286821c9bf
Emmanuel Onyekachi <kachirhaps@gmail.com>
2023-11-02 04:02:16 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * error_file - checks if files can be opened.
+ * @fd_source: fd_source
+ * @fd_dest: fd source
+ * @av: argument vector.
+ * Return: no return/
+ */
+void error_file(int fd_source, int fd_dest, char **av)
+{
+
+	if (fd_source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (fd_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
+
+/**
  * main - Entry point.
  * @argc: Argument counter.
  * @av: Argument vector.
@@ -9,35 +31,22 @@
  *
  * Return: Return 0 if successful.
  */
-
-void handle_error(int code, const char *message);
-
 int main(int argc, char **av)
 {
 	int fd_source, fd_dest;
 	char buffer[1024];
 	ssize_t n;
-
 	const char *file_from = av[1];
 	const char *file_to = av[2];
 
 	if (argc != 3)
-		handle_error(97, "Usage: cp file_from file_to");
-
+	{
+		dprintf(STDERR_FILENO, "%s\n", message);
+		exit(97);
+	}
 	fd_source = open(file_from, O_RDONLY);
-	if (fd_source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-
 	fd_dest = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fd_dest == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-
+	error_file(fd_source, fd_dest, av);
 	while ((n = read(fd_source, buffer, 1024)) > 0)
 	{
 		if (write(fd_dest, buffer, n) != n)
@@ -46,29 +55,20 @@ int main(int argc, char **av)
 			exit(99);
 		}
 	}
-
 	if (n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	if (close(fd_source) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_source);
 		exit(100);
 	}
-
 	if (close(fd_dest) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest);
 		exit(100);
 	}
-
 	return (0);
-}
-void handle_error(int code, const char *message)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
 }
